545bfc0e8b7cba8642d0688a9f99135b6f738a44
atiyabibi <167080885+atiyabibi@users.noreply.github.com>
2024-04-25 16:25:32 +0530

    Create LeftViewOfBinaryTree.py

---
+++ LeftViewOfBinaryTree.py
@@ -0,0 +1,50 @@
+#Left view of binary tree
+class TreeNode:
+    def __init__(self, data):
+        self.data = data 
+        self.left = None 
+        self.right = None 
+ 
+def leftViewOfBinaryTree(root):
+    result = []
+    Q = [root]
+ 
+    while len(Q) > 0:
+        n = len(Q)
+        for i in range(n):
+            # step-1: popping
+            curr = Q.pop(0)
+            if i == 0:
+                result.append(curr.data)
+ 
+            # step-2: pushing left child if its not none 
+            if curr.left != None:
+                Q.append(curr.left)
+ 
+            # step-3: pushing right child if its not none
+            if curr.right != None:
+                Q.append(curr.right)
+    print("Left view is:",result)
+
+root = TreeNode(18)
+obj2 = TreeNode(15)
+obj3 = TreeNode(20)
+obj4 = TreeNode(25)
+obj5 = TreeNode(30)
+obj6 = TreeNode(45)
+obj7 = TreeNode(80)
+ 
+root.left = obj2 
+root.right = obj3 
+ 
+obj2.left = obj4 
+obj2.right = obj5 
+ 
+obj3.left = obj6 
+obj3.right = obj7
+
+leftViewOfBinaryTree(root)
+
+
+Output
+Left view is: [18, 15, 25]
